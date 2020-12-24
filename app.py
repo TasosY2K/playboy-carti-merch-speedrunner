bde48bf7708ff8486912e6c9c925f2ae8182d54f
TasosY2K <gustavolama20@gmail.com>
2020-12-25 00:18:15 +0200

    lol

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print(1)
+from PIL import Image
+
+Image1 = Image.open('hoodie.png')
+
+Image1copy = Image1.copy()
+Image2 = Image.open('carti.jpg')
+Image2copy = Image2.copy()
+
+Image1copy.paste(Image2copy, (295, 225))
+Image1copy.save('merch.png')
+print("Done")
